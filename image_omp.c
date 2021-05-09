f8e1105b03ba35db9662e592e4e6d5561d68622c
Meghana Inabathini <inabath@br012.ib.bridges2.psc.edu>
2021-05-09 12:10:24 -0400

    OpenMP multi threads

--- image_omp.c
+++ image_omp.c
@@ -9,8 +9,8 @@
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
-#include <pthread.h>
-#define num 10
+#include <omp.h>
+#define num 100
 
 //An array of kernel matrices to be used for image convolution.  
 //The indexes of these match the enumeration from the header file. ie. algorithms[BLUR] returns the kernel corresponding to a box blur.
@@ -61,6 +61,7 @@ uint8_t getPixelValue(Image* srcImage,int x,int y,int bit,Matrix algorithm){
 void convolute(Image* srcImage,Image* destImage,Matrix algorithm){
     int row,pix,bit,span;
     span=srcImage->bpp*srcImage->bpp;
+#pragma omp for
     for (row=0;row<srcImage->height;row++){
         for (pix=0;pix<srcImage->width;pix++){
             for (bit=0;bit<srcImage->bpp;bit++){
@@ -113,7 +114,7 @@ int main(int argc,char** argv){
     destImage.height=srcImage.height;
     destImage.width=srcImage.width;
     destImage.data=malloc(sizeof(uint8_t)*destImage.width*destImage.bpp*destImage.height);
-    
+    #pragma omp parallel num_threads(num)
     convolute(&srcImage,&destImage,algorithms[type]);
     stbi_write_png("output.png",destImage.width,destImage.height,destImage.bpp,destImage.data,destImage.bpp*destImage.width);
     stbi_image_free(srcImage.data);
--- makefile
+++ makefile
@@ -1,5 +1,7 @@
-
+all: image image_omp
 image:image.c image.h
 	gcc -g image.c -o image -lm
+image_omp:image_omp.c image.h
+	gcc -g -fopenmp image_omp.c -o image_omp -lm
 clean:
-	rm -f image output.png
+	rm -f image image_omp output.png
